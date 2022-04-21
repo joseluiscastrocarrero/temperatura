316c41054268cd21aa539c0000d59d40b9e55347
joseluiscastrocarrero <josecastro1@colegioguanenta.edu.co>
2022-04-21 17:51:20 -0500

    Desarrollo: creacion de diagrama de flujo

---
+++ temperatura.py
@@ -0,0 +1,17 @@
+"Programa No. 3"
+"Programa para caulcular grados kelvin y celcius"
+
+print("--------------------")
+print("----grados celcius a farengeit y a kelvin----")
+print("--------------------")
+
+#input
+
+c=int(input("Dijite los grados"))
+
+#processing
+k=c+273.15
+f=c*1.8+32
+
+#ouput
+print(str(c)+" grados celcios son "+str(k)+" grados kelvin y "+str(f)+" grados frengeit")
